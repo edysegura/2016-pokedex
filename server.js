8a545d77e4c284d7abdbcf44446f49766486b872
Edy Segura <ednardo@inatel.br>
2016-11-17 16:56:44 -0200

    Server refactor

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ app.use(express.static('public'))
 
 //front-end
 app.get("/", (request, response) => {
-    response.sendFile("./index.html")
+    response.sendFile("index.html")
 })
 
-const server = app.listen(3000, _ => console.log("Server running at http://localhost:3000"))
+//start server
+const hostname = "localhost"
+const port = 3000
+const server = app.listen(port, _ => console.log(`Server running at http://${hostname}:${port}`))
